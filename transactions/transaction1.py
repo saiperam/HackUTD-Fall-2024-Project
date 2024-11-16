80f8acae6110926d4fcc79e5855f4793487c8a4d
Sai Peram <sai11.peram@gmail.com>
2024-11-16 17:04:13 -0600

    Added transaction prediction scripts and related files

---
+++ transactions/transaction1.py
@@ -0,0 +1,35 @@
+
+# Example of new transaction data (this should match the structure of your training data)
+new_transaction = {
+    'Time': 41505.0,    # Original Time value
+    'V1': -16.526507,   # Original V1 value
+    'V2': 8.584972,     # Original V2 value
+    'V3': -18.649853,   # Original V3 value
+    'V4': 9.505594,     # Original V4 value
+    'V5': -13.793819,   # Original V5 value
+    'V6': -2.832404,    # Original V6 value
+    'V7': -16.701694,   # Original V7 value
+    'V8': 7.517344,     # Original V8 value
+    'V9': -8.507059,    # Original V9 value
+    'V10': -14.110184,  # Original V10 value
+    'V11': 5.299236,    # Original V11 value
+    'V12': -10.834006,  # Original V12 value
+    'V13': 1.671120,    # Original V13 value
+    'V14': -9.373859,   # Original V14 value
+    'V15': 0.360806,    # Original V15 value
+    'V16': -9.899247,   # Original V16 value
+    'V17': -19.236292,  # Original V17 value
+    'V18': -8.398552,   # Original V18 value
+    'V19': 3.101735,    # Original V19 value
+    'V20': -1.514923,   # Original V20 value
+    'V21': 1.190739,    # Original V21 value
+    'V22': -1.127670,   # Original V22 value
+    'V23': -2.358579,   # Original V23 value
+    'V24': 0.673461,    # Original V24 value
+    'V25': -1.413700,   # Original V25 value
+    'V26': -0.462762,   # Original V26 value
+    'V27': -2.018575,   # Original V27 value
+    'V28': -1.042804,   # Original V28 value
+    'Amount': 364.19,   # Original Amount value
+
+}
---
+++ transactions/transaction2.py
@@ -0,0 +1,33 @@
+# transaction2.py
+new_transaction = {
+    'Time': 42000.0,    # Time value (could be in seconds since midnight, for example)
+    'V1': 1.356753,     # Some value representing the first feature
+    'V2': -3.432081,    # Some value representing the second feature
+    'V3': 12.127398,    # Some value representing the third feature
+    'V4': -8.432215,    # Some value representing the fourth feature
+    'V5': 5.787953,     # Some value representing the fifth feature
+    'V6': 3.164845,     # Some value representing the sixth feature
+    'V7': -2.537862,    # Some value representing the seventh feature
+    'V8': 0.263563,     # Some value representing the eighth feature
+    'V9': -4.978526,    # Some value representing the ninth feature
+    'V10': 6.434918,    # Some value representing the tenth feature
+    'V11': -1.987653,   # Some value representing the eleventh feature
+    'V12': 2.572123,    # Some value representing the twelfth feature
+    'V13': -9.372846,   # Some value representing the thirteenth feature
+    'V14': 5.032986,    # Some value representing the fourteenth feature
+    'V15': 0.763309,    # Some value representing the fifteenth feature
+    'V16': -4.678191,   # Some value representing the sixteenth feature
+    'V17': 9.438937,    # Some value representing the seventeenth feature
+    'V18': -1.122837,   # Some value representing the eighteenth feature
+    'V19': -6.745138,   # Some value representing the nineteenth feature
+    'V20': 2.347632,    # Some value representing the twentieth feature
+    'V21': -0.923467,   # Some value representing the twenty-first feature
+    'V22': 3.018547,    # Some value representing the twenty-second feature
+    'V23': 7.358432,    # Some value representing the twenty-third feature
+    'V24': -1.586734,   # Some value representing the twenty-fourth feature
+    'V25': 1.547623,    # Some value representing the twenty-fifth feature
+    'V26': -0.273645,   # Some value representing the twenty-sixth feature
+    'V27': 0.987463,    # Some value representing the twenty-seventh feature
+    'V28': -3.284597,   # Some value representing the twenty-eighth feature
+    'Amount': 150.75,    # Amount value (not too high, seems like a normal transaction)
+}
